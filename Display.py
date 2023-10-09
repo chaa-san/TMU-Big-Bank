7f1131382290056a2b0fad1fd3a75112d694339d
Cheska <75390042+chaa-san@users.noreply.github.com>
2023-10-08 23:56:49 -0400

    Delete Display.py

--- Display.py
+++
@@ -1,7 +0,0 @@
-class Display:
-    def showMessageLine(self, message):
-        print(message)
-
-    # displaying dollar amount
-    def showDollarAmount(self, amount):
-        print('${:,.2f}'.format(amount)) 
