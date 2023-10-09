e7f415e46ec2d1424670ebe10f3650f6067173b2
Cheska <75390042+chaa-san@users.noreply.github.com>
2023-10-08 23:56:58 -0400

    Delete Keypad.py

--- Keypad.py
+++
@@ -1,8 +0,0 @@
-class Keypad:
-    def __init__(self):
-        # reading data from the command line
-        self.input = input
-
-    # returning the integer value user inputed to sign-in
-    def get_input(self):
-        return int(input())
